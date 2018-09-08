c19799acd3eab8e1541ac3ee93d0203a87e1c76c
Inaki Abadia <i.abadiaosta@gmail.com>
2018-09-08 01:04:15 +0100

    Adds hdesk matrix mapping

---
+++ keyboards/hdesk/help_desk.h
@@ -0,0 +1,43 @@
+/*
+Copyright 2018 Inaki Abadia <i.abadiaosta@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef HELP_DESK_H
+#define HELP_DESK_H
+
+#include "quantum.h"
+
+/* HelpDesk
+ * ╔═══════╦═══════╦═══════╗
+ * ║  Mute ║ Vol ↓ ║ Vol ↑ ║
+ * ╠═══════╬═══════╬═══════╣
+ * ║  ◄ ◄  ║  ► ►  ║ Play  ║
+ * ╠═══════╩═══════╣       ║
+ * ║  Lock screen  ║ Pause ║
+ * ╚═══════════════╩═══════╝
+ */
+
+#define LAYOUT( \
+      K00, K01, K02, \
+      K10, K11, K12, \
+      K20  \
+) \
+{ \
+    { K00, K01, K02 }, \
+    { K10, K11, K12 }, \
+    { K20 } \
+}
+
+#endif
